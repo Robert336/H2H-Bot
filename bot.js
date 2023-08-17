c739d9f1f9a5b0a23a9f1329d3775cb7933929e6
Robert336 <rmazza33@gmail.com>
2023-08-16 22:02:31 -0400

    setup slash commands

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
-
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [
 	GatewayIntentBits.Guilds,
@@ -11,25 +11,27 @@ const client = new Client({ intents: [
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	}
-	else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		}
+		else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
 	}
 }
 
 // Set your desired command prefix
 // const prefix = '!';
-
-const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
@@ -53,10 +55,7 @@ client.on(Events.InteractionCreate, async interaction => {
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 		}
 	}
-
-	console.log(interaction);
 });
-
 
 // Replace 'YOUR_BOT_TOKEN' with your actual bot token
 client.login(DISCORD_BOT_TOKEN);
